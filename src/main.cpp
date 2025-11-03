07e1d615393f945ba1167e944e47f1fc8a43e1bb
To0nsa <nicolas.lovis@hotmail.fr>
2025-11-03 09:28:33 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+#include "ResourceManager.h"
+#include "gamestates/StateStack.h"
+#include "gamestates/IState.h"
+#include "gamestates/StateMenu.h"
+#include <memory>
+#include <stack>
+#include <optional>
+#include <SFML/Graphics.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/System/Time.hpp>
+
+int main(int argc, char* argv[])
+{
+    (void)argc;
+
+    // ResourceManager must be instantiated here -- DO NOT CHANGE
+    ResourceManager::init(argv[0]);
+
+    sf::RenderWindow window(sf::VideoMode({1024, 1024}), "Runner");
+    window.setKeyRepeatEnabled(false);
+    window.setVerticalSyncEnabled(true);
+
+    StateStack gamestates;
+    if (!gamestates.push<StateMenu>())
+        return -1;
+
+    sf::Clock clock;
+    while (window.isOpen())
+    {
+        sf::Time elapsedTime = clock.restart();
+
+        IState* pState = gamestates.getCurrentState();
+        if (!pState) return -1;
+
+        while (const std::optional<sf::Event> event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+
+        pState->update(elapsedTime.asSeconds());
+        window.clear();
+        pState->render(window);
+        window.display();
+
+        gamestates.performDeferredPops();
+    }
+    
+    return 0;
+}
